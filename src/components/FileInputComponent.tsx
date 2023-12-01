c85183ef35274424ed9e53fabf93c355c852ccb0
Ludovic Descateaux <ludovicd@hokla.com>
2023-12-01 09:12:04 +0100

    fixup separate file input component

--- src/components/FileInputComponent.tsx
+++ src/components/FileInputComponent.tsx
@@ -1,21 +1,30 @@
-import FileUpload from "./FileUploadComponent";
+import { ChangeEvent, RefObject } from "react";
 
 type FileInputProps = {
   objectKey: string;
   progress: number;
   handleFileSelect: (e: FileList | null) => void;
+  fileInputRef: RefObject<HTMLInputElement>;
 };
 
 export const FileInputComponent = ({
   objectKey,
   progress,
   handleFileSelect,
+  fileInputRef,
 }: FileInputProps) => {
   return (
     <div className="fileInput">
       <h3 className="title">Normal R1:</h3>
       <div className="uploadProgress">
-        <FileUpload className="rawInput" handleOnselect={handleFileSelect} />
+        <input
+          className="rawInput"
+          type="file"
+          onChange={(e: ChangeEvent) =>
+            handleFileSelect((e?.target as HTMLInputElement)?.files)
+          }
+          ref={fileInputRef}
+        />
         <p>Upload Progress: {progress === 0 ? "" : `${progress} %`}</p>
       </div>
       <label className="objectKey">File key in the datalake: {objectKey}</label>
